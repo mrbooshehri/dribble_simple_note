62f07357fab1621c7f35eedc34da5837d35033c8
mrbooshehri <mrbooshehri@gmail.com>
2020-11-20 17:48:43 +0300

    note list added

--- src/Components/noteList.jsx
+++ src/Components/noteList.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import NoteListItem from "./noteListItem";
+
+let note = {
+  title: "title area",
+  body: "there will be some text here:)",
+  time: "12 Nov",
+};
 class NoteList extends Component {
   state = {};
   render() {
-    return <div className="col-4"></div>;
+    return (
+      <div className="col-4 p-0 noteList">
+        <NoteListItem note={note} />
+        <NoteListItem note={note} />
+        <NoteListItem note={note} />
+        <NoteListItem note={note} />
+        <NoteListItem note={note} />
+        <NoteListItem note={note} />
+        <NoteListItem note={note} />
+        <NoteListItem note={note} />
+      </div>
+    );
   }
 }
 
--- src/Components/noteListItem.jsx
+++ src/Components/noteListItem.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 class NoteListItem extends Component {
   state = {};
-  render() {
-    return <div></div>;
+  render(props) {
+    return (
+      <div className="noteListItem">
+        <div className="noteListItem__header">
+          <h4 className="noteListItem__header__title">
+            {this.props.note.title}
+          </h4>
+          <h6 className="noteListItem__header__time">{this.props.note.time}</h6>
+        </div>
+        <p className="noteListItem__body">{this.props.note.body}</p>
+      </div>
+    );
   }
 }
 
